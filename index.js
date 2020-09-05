813a62925a0a85f0fdd594cc664be32b1bf1da96
mvampa91 <67586944+mvampa91@users.noreply.github.com>
2020-09-05 17:42:13 +0200

    Commit

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const server = http.createServer(app);
-const webrtc = io(server);
-
-webrtc.on('connection', socket => {
-    socket.on('message', ({name, message}))
-})
-
-// Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
 // All remaining requests return the React app, so it can handle routing.
@@ -22,6 +14,18 @@ app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
-app.listen(PORT, function () {
+const server = http.createServer(app);
+const webrtc = io(server);
+
+webrtc.set('origins', '*:*');
+webrtc.on('connection', socket => {
+    socket.on('message', ({score, message, id}) => {
+      webrtc.emit('message', {score, message, id})
+    })
+})
+
+// Priority serve any static files.
+
+server.listen(PORT, function () {
   console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
 });
